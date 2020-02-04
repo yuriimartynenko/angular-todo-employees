d43b226fb8788ba0f6d7d1e91877cdcb8fb7bd6d
Yurii Martynenko <ymartinenko4@gmail.com>
2020-02-04 18:55:26 +0200

    delete all files

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from 'angular-ts-decorators';
-import { AppModule } from './app/app.module';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/js/all';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
